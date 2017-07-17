9b0db4d8e6cb7648ac556bfcacad11f9cca3f03f
liwd <619882632@qq.com>
2017-07-17 20:39:22 +0800

    完成基本的登录注册界面ui，完成基本的路由功能，新增一部分全局样式定制

---
+++ src/components/BtnComponent.js
@@ -0,0 +1,23 @@
+/**
+ * Created by mapbar_front on 2017/7/17.
+ */
+import React, { Component } from 'react';
+import {
+    Text,
+    TouchableOpacity
+} from 'react-native';
+
+import Env from '../Env';
+const estyle = Env.style;
+
+export default class Btn extends Component{
+    render(){
+        return (
+            <TouchableOpacity onPress={()=>{
+                this.props.OnClick && this.props.OnClick();
+            }} style={[estyle.btn_base]}>
+                <Text style={[estyle.font_h2,estyle.font_color_white]}>登录</Text>
+            </TouchableOpacity>
+        )
+    }
+}
